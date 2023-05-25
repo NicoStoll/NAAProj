6e14002f14cff6195363026666d2102f0e578b35
Özer, Kaan <kaan.oezer@study.thws.de>
2023-05-25 11:04:26 +0200

    user services added

--- NAAProject/NAAProject.Services/Service/UserService.cs
+++ NAAProject/NAAProject.Services/Service/UserService.cs
@@ -3,11 +3,34 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NAAProject.Data.Models.DAO;
+using NAAProject.Data.Models.Domain;
+using NAAProject.Data.Models.IDAO;
+using NAAProject.Data.Models.Repository;
 using NAAProject.Services.IService;
 
 namespace NAAProject.Services.Service
 {
     public class UserService : IUserService
     {
-    }
+
+        IUsersDAO userDAO;
+        public UserService()
+        {
+            userDAO = new UserDAO();
+        }
+        public User GetUser(Application application)
+        {
+            using (NAAContext context = new NAAContext()) {
+                return userDAO.GetUser(application, context);
+            }
+        }
+
+        public IList<User> GetUsers()
+        {
+            using (NAAContext context = new NAAContext())
+            {
+                return userDAO.GetUsers(context);
+            }
+        }
 }
