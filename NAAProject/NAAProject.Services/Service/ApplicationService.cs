de0c0901f2312c400ee69081bcfadf41e5fdf6b3
MiBB3L <81522869+MiBB3L@users.noreply.github.com>
2023-05-25 11:24:55 +0200

    Add files via upload

--- NAAProject/NAAProject.Services/Service/ApplicationService.cs
+++ NAAProject/NAAProject.Services/Service/ApplicationService.cs
@@ -3,11 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NAAProject.Data.Models.DAO;
+using NAAProject.Data.Models.Domain;
+using NAAProject.Data.Models.IDAO;
+using NAAProject.Data.Models.Repository;
 using NAAProject.Services.IService;
 
 namespace NAAProject.Services.Service
 {
     public class ApplicationService : IApplicationService
     {
+        IApplicationDAO applicationDAO;
+        public ApplicationService()
+        {
+            applicationDAO = new ApplicationDAO();
+        }
+        public Application GetApplication(int id)
+        {
+            using (NAAContext context = new NAAContext())
+            {
+                Application application = applicationDAO.GetApplication(context, id);
+                return application;
+            }
+        }
+
+        public IList<Application> GetApplications()
+        {
+            using (NAAContext context = new NAAContext())
+            {
+                return applicationDAO.GetApplications(context);
+            }
+        }
     }
 }
