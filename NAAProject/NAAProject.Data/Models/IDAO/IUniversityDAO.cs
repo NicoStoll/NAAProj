1fc0671da8af499a54b1a91c11a6496f2f8c1026
MiBB3L <81522869+MiBB3L@users.noreply.github.com>
2023-05-25 11:27:00 +0200

    Add files via upload

--- NAAProject/NAAProject.Data/Models/IDAO/IUniversityDAO.cs
+++ NAAProject/NAAProject.Data/Models/IDAO/IUniversityDAO.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NAAProject.Data.Models.Domain;
+using NAAProject.Data.Models.Repository;
 
 namespace NAAProject.Data.Models.IDAO
 {
     public interface IUniversityDAO
     {
+        IList<University> GetUniversities(NAAContext context);
+        University GetUniversity(NAAContext context, int id);
+        void AddUniversity(NAAContext context, University university);
+        void DeleteUniversity(NAAContext context, University university);
+        void UpdateUniversity(NAAContext context, University university);
     }
 }
