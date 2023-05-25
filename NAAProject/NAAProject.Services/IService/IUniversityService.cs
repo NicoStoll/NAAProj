b01e0cd4d03b75971319b769010cc2e7f16fffa0
MiBB3L <81522869+MiBB3L@users.noreply.github.com>
2023-05-25 11:25:34 +0200

    Add files via upload

--- NAAProject/NAAProject.Services/IService/IUniversityService.cs
+++ NAAProject/NAAProject.Services/IService/IUniversityService.cs
@@ -3,10 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NAAProject.Data.Models.Domain;
 
 namespace NAAProject.Services.IService
 {
     public interface IUniversityService
     {
+        IList<University> GetuUniversities();
+        University GetUniversity(int id);
     }
 }
